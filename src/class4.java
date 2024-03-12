461f22a1bd940e9b12550be5b68271263f55fbd3
aditya-7562 <aditya.12114424@lpu.in>
2024-03-12 16:36:18 +0530

    committed first assignment partially

---
+++ src/class4.java
@@ -0,0 +1,11 @@
+public class class4 {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 7;
+
+        System.out.println("X is: " + x);
+        System.out.println("Y is: " + y);
+
+        
+    }
+}

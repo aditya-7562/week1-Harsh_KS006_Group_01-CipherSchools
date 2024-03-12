97ccb77bcf2296a6db79a0340db22152bc645807
aditya-7562 <aditya.12114424@lpu.in>
2024-03-12 16:44:50 +0530

    completed class 4 assignment

--- src/class4.java
+++ src/class4.java
@@ -6,6 +6,19 @@ public class class4 {
         System.out.println("X is: " + x);
         System.out.println("Y is: " + y);
 
+        byte b = 10;
+        System.out.println("B is: " + b);
+
+        byte b2 = (byte)129;
+        System.out.println("B2 is: " + b2);
         
+        byte b3 = (byte)128;
+        System.out.println("B3 is: " + b3);
+
+        byte b4 = (byte)256;
+        System.out.println("B4 is: " + b4);
+
+        int i = b;
+        System.out.println("I is: " + i);
     }
 }

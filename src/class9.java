9cea5f56e9cf7c0916f5f0a407ebc928185b39fb
aditya-7562 <aditya.12114424@lpu.in>
2024-03-15 22:08:03 +0530

    done with the class 10 assignment

--- src/class9.java
+++ src/class9.java
@@ -1,40 +1,40 @@
-class A{
-    int a, b;
+// class A{
+//     int a, b;
 
-    public void show(){
-        System.out.println("Hello!");
-    }
-}
+//     public void show(){
+//         System.out.println("Hello!");
+//     }
+// }
 
-interface B{
-    public void eat();
-}
+// interface B{
+//     public void eat();
+// }
 
-interface D extends B{
-    public void sleep();
-}
+// interface D extends B{
+//     public void sleep();
+// }
 
-class C implements B, D{
-    int e, f;
-    public void speak(){
-        System.out.println("Speaking!");
-    }
+// // class C implements B, D{
+//     int e, f;
+//     public void speak(){
+//         System.out.println("Speaking!");
+//     }
 
-    @Override
-    public void eat(){
+//     @Override
+//     public void eat(){
         
-    }
+//     }
 
-    @Override
-    public void sleep(){
+//     @Override
+//     public void sleep(){
         
-    }
-}
+//     }
+// }
 
-public class class9 {
-    public static void main(String[] args) {
-        // Single Inheritance
-        B b = new B();
-        C c = new C();
-    }
-}
+// public class class9 {
+//     public static void main(String[] args) {
+//         // Single Inheritance
+//         // B b = new B();
+//         // C c = new C();
+//     }
+// }

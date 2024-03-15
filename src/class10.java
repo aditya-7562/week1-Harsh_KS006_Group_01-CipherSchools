fdab6668bd8cb61b83fa80e3e0ed80be8c0d3620
aditya-7562 <aditya.12114424@lpu.in>
2024-03-15 22:16:58 +0530

    done with class 11 assignment

--- src/class10.java
+++ src/class10.java
@@ -1,28 +1,28 @@
-class Student{
-    int rollNo;
-    String name;
-    static int count = 0;
+// class Student{
+//     int rollNo;
+//     String name;
+//     static int count = 0;
 
-    Student(int rollNo, String name){
-        this.rollNo = rollNo;
-        this.name = name;
-        count++;
-    }
+//     Student(int rollNo, String name){
+//         this.rollNo = rollNo;
+//         this.name = name;
+//         count++;
+//     }
 
-    static void showCount(){
-        System.out.println(count);
-    }
-}
+//     static void showCount(){
+//         System.out.println(count);
+//     }
+// }
 
-public class class10 {
-    public static void main(String[] args) {
-       Student s1 = new Student(1, "Ram");
-       System.out.println(s1.count); 
-       Student s2 = new Student(2, "Sham");
-       System.out.println(s2.count); 
-       Student s3 = new Student(3, "Alex");
-       System.out.println(s3.count); 
+// public class class10 {
+//     public static void main(String[] args) {
+//        Student s1 = new Student(1, "Ram");
+//        System.out.println(s1.count); 
+//        Student s2 = new Student(2, "Sham");
+//        System.out.println(s2.count); 
+//        Student s3 = new Student(3, "Alex");
+//        System.out.println(s3.count); 
 
-       System.out.println(Student.count);
-    }
-}
+//        System.out.println(Student.count);
+//     }
+// }
---
+++ src/class11.java
@@ -0,0 +1,26 @@
+class A{
+    final int noOfAlphabets = 26;
+    final int noOfDigits;
+    A(){
+        noOfDigits = 10;
+    }
+    final void fun(){
+        System.out.println("Hi, I'm a function in class A");
+    }
+}
+
+final class B extends A{
+    // void fun(){
+    //     System.out.println("Hi, I'm a function in class A");
+    // }
+}
+
+// class C extends B{
+
+// }
+
+public class class11 {
+    public static void main(String[] args) {
+        
+    }
+}

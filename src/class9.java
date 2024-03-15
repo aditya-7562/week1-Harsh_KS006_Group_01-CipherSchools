8b4219f724d156715b9200f23d7951700dc98bf4
aditya-7562 <aditya.12114424@lpu.in>
2024-03-15 21:49:32 +0530

    done with the class 9 assignment

---
+++ src/class9.java
@@ -0,0 +1,40 @@
+class A{
+    int a, b;
+
+    public void show(){
+        System.out.println("Hello!");
+    }
+}
+
+interface B{
+    public void eat();
+}
+
+interface D extends B{
+    public void sleep();
+}
+
+class C implements B, D{
+    int e, f;
+    public void speak(){
+        System.out.println("Speaking!");
+    }
+
+    @Override
+    public void eat(){
+        
+    }
+
+    @Override
+    public void sleep(){
+        
+    }
+}
+
+public class class9 {
+    public static void main(String[] args) {
+        // Single Inheritance
+        B b = new B();
+        C c = new C();
+    }
+}

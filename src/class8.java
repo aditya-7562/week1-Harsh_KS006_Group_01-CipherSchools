b1b50e8e74e29a18f9b2b55adb435cc986088435
aditya-7562 <aditya.12114424@lpu.in>
2024-03-15 21:27:33 +0530

    completed class 8 assignment

---
+++ src/class8.java
@@ -0,0 +1,102 @@
+
+// class Car {
+//     String name;
+//     String color;
+//     int year;
+//     int maxSpeed;
+//     void accelerate(){
+//         System.out.println("Car is accelerating");
+//     }
+// }
+
+// public class class8 {
+//     public static void main(String[] args) {
+//         Car polo = new Car();
+//         System.out.println(polo.name);
+//         System.out.println(polo.color);
+//         System.out.println(polo.year);
+//         System.out.println(polo.maxSpeed);
+        
+//         polo.name = "Polo";
+//         polo.color = "Red";
+//         polo.year = 2016;
+//         polo.maxSpeed = 120;
+        
+//         System.out.println(polo.name);
+//         System.out.println(polo.color);
+//         System.out.println(polo.year);
+//         System.out.println(polo.maxSpeed);
+
+
+//     }
+// }
+
+class Student{
+    String name;
+    int marks;
+
+    boolean checkPass(){
+        // if(marks>40){
+        //     return true;
+        // }else{
+        //     return false;
+        // }
+        if(marks>=40) return true;
+        return false;
+    }
+}
+
+class ControlFlow{
+    int findMax(int a, int b, int c){
+        if(a>b){
+            if(a>c){
+                return a;
+            }else{
+                return c;
+            }
+        }else{
+            if(b>c){
+                return b;
+            }else{
+                return c;
+            }
+        }
+    }
+}
+public class class8 {
+
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        s1.name = "Ram";
+        s1.marks = 80;
+
+        Student s2 = new Student();
+        s2.name = "Shyam";
+        s2.marks = 30;
+
+        System.out.println(s1.checkPass());
+        System.out.println(s2.checkPass());
+
+        ControlFlow cf = new ControlFlow();
+        System.out.println(cf.findMax(10,7,15));
+
+        char c = 'a';
+        switch(c){
+            case 'a':
+                System.out.println("Hi, I'm A");
+                break;
+            case 'b':
+                System.out.println("Hi, I'm B");
+                break;
+            case 'c':
+                System.out.println("Hi, I'm C");
+                break;
+            default:
+                System.out.println("I'm not A, B or C");
+                break;
+        }
+
+        System.out.println("After switch");
+
+    }
+}
